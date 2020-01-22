fe58e24bdd37b629bc47f0609da3c4c56fbb0b53
Harsh Udai <50704886+HarshUdai@users.noreply.github.com>
2020-01-23 00:21:58 +0530

    First Program to Write Hello World

---
+++ Hello_World.c
@@ -0,0 +1,15 @@
+/*
+Step 1: We use stdio.h header file to make a standard input and output.
+Step 2: We make a main function.
+Step 3: We use prinf to show out result 
+        Eg: let's say hello world.
+Step 4: We write a line return 0.
+        Because we declare our main function of int type i.e, we have to write return statement.
+
+*/
+#include <stdio.h>
+
+int main(){
+    printf("Hello World");
+    return 0;
+}
